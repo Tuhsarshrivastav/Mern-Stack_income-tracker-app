b0148dbbac7805b70ba66f64eab267bd3f485767
Tusharshrivastav <ts3657@gmail.com>
2021-10-25 13:35:03 +0530

    added user schama

---
+++ src/model/User.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+//schema for user
+
+const userschema = mongoose.Schema(
+  {
+    firstname: {
+      type: "String",
+      required: [true, "First name is required"],
+    },
+    lastname: {
+      type: "String",
+      required: [true, "Last name is required"],
+    },
+    email: {
+      type: "String",
+      required: [true, "Email is required"],
+    },
+    password: {
+      type: "String",
+      required: [true, "Password is required"],
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamp: true,
+  }
+);
+
+module.exports = mongoose.Model("User", userschema);
