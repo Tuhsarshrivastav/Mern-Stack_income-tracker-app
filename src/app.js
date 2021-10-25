dad20241352dc44e4adcedd6270f644831b6d5b2
Tusharshrivastav <ts3657@gmail.com>
2021-10-25 14:49:31 +0530

    added notfound errormidlleware

--- src/app.js
+++ src/app.js
@@ -1,22 +1,26 @@
+// dependencys
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const cors = require("cors")
+const cors = require("cors");
+
 // imports
 const dbConnect = require("./config/db.config");
 const userRouter = require("./routes/userRoute");
-const {errorHandler}= require("./middlewares/errorMiddlewares")
+const { errorHandler, notfound } = require("./middlewares/errorMiddlewares");
+
 // db configuration
 dbConnect();
 
 //middleware
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 //routes
 app.use("/api/users", userRouter);
 
 //error handlers
-app.use(errorHandler)
+app.use(notfound);
+app.use(errorHandler);
 
 module.exports = app;
--- src/middlewares/errorMiddlewares.js
+++ src/middlewares/errorMiddlewares.js
@@ -3,8 +3,15 @@ const errorHandler = (err, req, res, next) => {
   res.status(statusCode);
   res.json({
     msg: err.message,
-    stacks: process.env.NODE_ENV !== "production" ? null : err.stack,
+    stacks: process.env.NODE_ENV === "production" ? null : err.stack,
   });
 };
 
-module.exports = { errorHandler };
+//not found
+const notfound = (req, res, next) => {
+  const error = new Error(`not found -${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+module.exports = { errorHandler, notfound };
