d5220325fe87d3d4ec4c31a328293f03ea95e13d
Tusharshrivastav <ts3657@gmail.com>
2021-10-27 18:46:36 +0530

    added login register components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
-
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./screens/Home";
+import Login from "./screens/users/Login";
+import Register from "./screens/users/Register";
 function App() {
   return (
-    <div className="App">
-     hello
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Switch>
+    </Router>
   );
 }
 
