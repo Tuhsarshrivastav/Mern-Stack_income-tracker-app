8190f1cf2e955a8fd19bdc81d7cafc2987134d57
Tusharshrivastav <ts3657@gmail.com>
2021-10-27 12:53:57 +0530

    addd update and delete api in income

--- src/controllers/income/incomeController.js
+++ src/controllers/income/incomeController.js
@@ -36,8 +36,48 @@ const fetchSingleIncome = expressAsyncHandler(async (req, res) => {
     const income = await Income.findById(id);
     res.json(income);
   } catch (error) {
-    throw new Error(error + "Problem while fetch user income , please try again ");
+    throw new Error(
+      error + "Problem while fetch user income , please try again "
+    );
   }
 });
 
-module.exports = { createIncome, fetchincome, fetchSingleIncome };
+//update income api
+const updateIncome = expressAsyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const { title, description, amount } = req.body;
+  try {
+    const income = await Income.findByIdAndUpdate(
+      id,
+      {
+        title,
+        description,
+        amount,
+      },
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    res.json(income);
+  } catch (error) {
+    throw new Error(
+      error + "Problem while updating user income , please try again "
+    );
+  }
+});
+
+//delete income api
+const deleteIncome = expressAsyncHandler(async (req, res) => {
+  const { id } = req.params;
+  try {
+    const income = await Income.findByIdAndDelete(id);
+    res.json(income);
+  } catch (error) {
+    throw new Error(
+      error + "Problem while deleting user income , please try again "
+    );
+  }
+});
+
+module.exports = { updateIncome, createIncome, fetchincome, fetchSingleIncome ,deleteIncome};
--- src/routes/incomeRoute.js
+++ src/routes/incomeRoute.js
@@ -2,12 +2,16 @@ const express = require("express");
 const {
   createIncome,
   fetchincome,
-  fetchSingleIncome
+  fetchSingleIncome,
+  updateIncome,
+  deleteIncome
 } = require("../controllers/income/incomeController");
 const router = express();
 
 router.post("/", createIncome);
 router.get("/", fetchincome);
 router.get("/:id", fetchSingleIncome);
+router.put("/:id", updateIncome);
+router.put("/:id",deleteIncome);
 
 module.exports = router;
