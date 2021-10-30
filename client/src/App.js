e6b02b1a8bf1c3e409fa01f3e92b0bb6e2d95a1e
Tusharshrivastav <ts3657@gmail.com>
2021-10-30 11:36:19 +0530

    added Protected Route

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,19 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./screens/Home";
 import Login from "./screens/users/Login";
 import Register from "./screens/users/Register";
+import AddIncome from "./screens/income/AddIncome";
+import AddExpense from "./screens/expense/AddExpense";
+import Profile from "./screens/users/Profile";
+import PrivateProtectRoute from "./components/Navigation/PrivateProtectRoute";
+
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Home} />
+        <PrivateProtectRoute exact path="/add-income" component={AddIncome} />
+        <PrivateProtectRoute exact path="/add-expense" component={AddExpense} />
+        <PrivateProtectRoute exact path="/profile" component={Profile} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
       </Switch>
